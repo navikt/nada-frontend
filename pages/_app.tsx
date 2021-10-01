e25fdd00ac089f541790d91492077f73827640da
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-10-01 15:05:57 +0200

    UPDATE: to use context for search and user state
    Co-authored-by: frodesundby  <frode.sundby@nav.no>
    Co-authored-by: Christine Teig <christine.teig@nav.no>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@navikt/ds-css'
+import { createContext, useState } from 'react'
+
+type SearchStateType = {
+  query: string
+  setQuery: Function
+}
+
+type UserState = {
+  user: string
+  setUser: Function
+}
+
+export const SearchState = createContext<SearchStateType>({ query: '', setQuery: () => { } })
+export const UserState = createContext<UserState>({ user: '', setUser: () => { } })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [searchState, setSeachState] = useState('')
+  const [userState, setUserState] = useState('')
+
+  return (
+    <UserState.Provider value={{ user: userState, setUser: setUserState }}>
+      <SearchState.Provider value={{ query: searchState, setQuery: setSeachState }}>
+        <Component {...pageProps} />
+      </SearchState.Provider>
+    </UserState.Provider>
+  )
 }
 export default MyApp
