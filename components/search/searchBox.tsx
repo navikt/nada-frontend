74bc76b2bb850c5de3f7c6bdcfdc65ffcc98aeaf
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-04 14:33:13 +0200

    more CSS improvements

--- components/search/searchBox.tsx
+++ components/search/searchBox.tsx
@@ -1,52 +1,60 @@
 import styled from 'styled-components'
-import {Button, TextField} from '@navikt/ds-react'
-import React, {useContext} from 'react'
-import {SearchState} from '../../pages/_app';
-import {useRouter} from "next/router";
-import {Search} from "@navikt/ds-icons";
+import { Button, TextField } from '@navikt/ds-react'
+import React, { useContext } from 'react'
+import { SearchState } from '../../pages/_app'
+import { useRouter } from 'next/router'
+import { Search } from '@navikt/ds-icons'
 
 const SearchDiv = styled.div`
-  width: 80%;
+  width: 70%;
   padding: 15px;
   margin: 0 auto;
   .navds-form-field {
     width: 100%;
-  };
+  }
+  .navds-button {
+    padding: 0 2em 0 1em;
+    margin-left: 1em;
+    border-radius: 0.25em;
+  }
 `
 
 export default function SearchBox() {
+  const searchState = useContext(SearchState)
+  const router = useRouter()
 
-    const searchState = useContext(SearchState)
-    const router = useRouter()
-
-    return (
-        <SearchDiv role="navigation">
-            <div style={{display: "flex",}}>
-                <TextField label="" hideLabel
-                           value={searchState.value}
-                           onChange={(e) => {
-                               searchState.setValue(e.target.value)
-                           }}
-                           onKeyDown={(event) => {
-                               if (event.key === 'Enter') {
-                                   searchState.setQuery(searchState.value)
-                                   router.push({
-                                       pathname: '/search',
-                                       query: {q: searchState.value}
-                                   })
-                               }
-                           }}
-                />
-                <Button onClick={() => {
-                    searchState.setQuery(searchState.value)
-                    router.push({
-                        pathname: '/search',
-                        query: {q: searchState.query}
-                    })
-                }}>
-                    <Search/> Søk
-                </Button>
-            </div>
-        </SearchDiv>
-    )
+  return (
+    <SearchDiv role="navigation">
+      <div style={{ display: 'flex' }}>
+        <TextField
+          label=""
+          hideLabel
+          value={searchState.value}
+          onChange={(e) => {
+            searchState.setValue(e.target.value)
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              searchState.setQuery(searchState.value)
+              router.push({
+                pathname: '/search',
+                query: { q: searchState.value },
+              })
+            }
+          }}
+        />
+        <Button
+          onClick={() => {
+            searchState.setQuery(searchState.value)
+            router.push({
+              pathname: '/search',
+              query: { q: searchState.query },
+            })
+          }}
+        >
+          <Search /> Søk
+        </Button>
+      </div>
+    </SearchDiv>
+  )
 }
--- pages/api/dataproducts/[id].ts
+++ pages/api/dataproducts/[id].ts
@@ -1,36 +1,37 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {DataproductSchema} from '../../../lib/schema_types'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { DataproductSchema } from '../../../lib/schema_types'
 
 const response: DataproductSchema = {
-    id: 'test',
-    name: 'Team Risk',
-    description: `## Team Risk – regelanalyse
-    Brukes som beslutningsgrunnlag i forbindelse med utvikling av en risk-komponent i ny sykepengeløsning.`,
-    slug: 'lorem ipsum',
-    repo: 'lorem ipsum',
-    last_modified: '2013-09-10T19:00Z',
-    created: '1887-08-21T19:00Z',
-    owner: {
-        team: 'Test team',
-        teamkatalogen: 'Test team',
+  id: 'test',
+  name: 'Team Risk',
+  description:
+    `## Team Risk – regelanalyse\n` +
+    `Brukes som beslutningsgrunnlag i forbindelse med utvikling av en risk-komponent i ny sykepengeløsning.`,
+  slug: 'lorem ipsum',
+  repo: 'lorem ipsum',
+  last_modified: '2013-09-10T19:00Z',
+  created: '1887-08-21T19:00Z',
+  owner: {
+    team: 'Test team',
+    teamkatalogen: 'Test team',
+  },
+  keywords: ['lorem ipsum', 'sorryyy'],
+  datasets: [
+    {
+      id: 'DS_1',
+      name: 'Treff på regler over tid',
+      type: 'bigquery',
     },
-    keywords: ['lorem ipsum', 'sorryyy'],
-    datasets: [
-        {
-            id: 'DS_1',
-            name: 'Treff på regler over tid',
-            type: 'bigquery',
-        },
-        {
-            id: 'DS_2',
-            name: 'Kumulativt antall saker',
-            type: 'bigquery',
-        },
-    ],
+    {
+      id: 'DS_2',
+      name: 'Kumulativt antall saker',
+      type: 'bigquery',
+    },
+  ],
 }
 
 export const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json(response)
+  res.status(200).json(response)
 }
 
 export default handler
