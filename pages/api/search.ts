7d9588f898376c79d9193f75c53ae57f27e84bd9
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-04 14:27:05 +0200

    better test data

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -8,53 +8,61 @@ const response: SearchResultEntry[] = [
     url: 'banan',
     type: 'dataproduct',
     id: 'dp_1',
-    name: 'DP 1',
-    excerpt: 'Dette er en lang og fin beskrivelse av det første produktet, som stopper etter... ',
+    name: 'Dataprodukt 1',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av det første produktet, som stopper etter... ',
   },
   {
     url: 'kake',
     type: 'dataproduct',
     id: 'dp_2',
-    name: 'DP 2',
-    excerpt: 'Dette er en lang og fin beskrivelse av det andre produktet, som stopper etter... ',
+    name: 'Dataprodukt 2',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av det andre produktet, som stopper etter... ',
   },
   {
     url: 'Shake',
     type: 'dataset',
     id: 'ds_1',
-    name: 'DS 1',
-    excerpt: 'Dette er en lang og fin beskrivelse av det første datasettet, som stopper etter... ',
+    name: 'Datasett 1',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av det første datasettet, som stopper etter... ',
   },
   {
     url: 'Shake',
     type: 'dataset',
     id: 'ds_2',
-    name: 'DS 2',
-    excerpt: 'Dette er en lang og fin beskrivelse av det første datasettet, som stopper etter... ',
+    name: 'Datasett 2',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av det første datasettet, som stopper etter... ',
   },
   {
     url: 'Cookie',
     type: 'datapackage',
     id: 'DPA_1',
-    name: 'DPA 1',
-    excerpt: 'Dette er en lang og fin beskrivelse av det første pakke, som stopper etter... ',
+    name: 'Datapakke 1',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av det første pakke, som stopper etter... ',
   },
   {
     url: 'Cookie',
     type: 'datapackage',
     id: 'DPA_2',
-    name: 'DPA 2',
-    excerpt: 'Dette er en lang og fin beskrivelse av den andre pakke, som stopper etter... ',
+    name: 'Datapakke 2',
+    excerpt:
+      'Dette er en lang og fin beskrivelse av den andre pakke, som stopper etter... ',
   },
 ]
 
 export const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const query: string | string[] = req.query["q"]
+  const query: string | string[] = req.query['q']
 
-
-  let filteredResponse  = response.filter((r) =>
-      Object.values(r).join().toLowerCase().includes(
-          query instanceof Array ? query[0] : query))
+  let filteredResponse = response.filter((r) =>
+    Object.values(r)
+      .join()
+      .toLowerCase()
+      .includes(query instanceof Array ? query[0] : query)
+  )
 
   res.status(200).json(filteredResponse)
 }
