b8a7a1e19c46823d90150b296d8bb4030ff97ca0
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-05 13:09:11 +0200

    Add /api prefix to fetches

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,5 @@
 export const fetcher = async (url: string) => {
-  const res = await fetch(url)
+  const res = await fetch(`api/${url}`)
   if (!res.ok) {
     const error = new Error(`${res.status} - ${res.statusText}`)
     throw error
