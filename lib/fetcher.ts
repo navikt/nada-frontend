4f0ed40426b233e0c9bbcb4a1f78f57a15b359cd
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-05 12:44:40 +0200

    Strategic retreat from env-based configuration

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,10 +1,9 @@
 export const fetcher = async (url: string) => {
-    const api = process.env.NEXT_PUBLIC_BACKEND
-    const res = await fetch(`${api}${url}`)
-    if (!res.ok) {
-        const error = new Error(`${res.status} - ${res.statusText}`)
-        throw error
-    }
-    return res.json()
+  const res = await fetch(url)
+  if (!res.ok) {
+    const error = new Error(`${res.status} - ${res.statusText}`)
+    throw error
+  }
+  return res.json()
 }
 export default fetcher
