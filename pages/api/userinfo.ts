de8cfe34cc6092eed127162a2373e54514c9e196
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-05 12:28:53 +0200

    reformat

--- pages/api/userinfo.ts
+++ pages/api/userinfo.ts
@@ -1,16 +1,16 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {UserInfoSchema} from '../../lib/schema_types'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { UserInfoSchema } from '../../lib/schema_types'
 
 const response: UserInfoSchema = {
-    name: "Bobby Brown",
-    email: "bobby.brown@nav.no",
-    teams: [
-        "nada", "knada"
-    ]
+  name: 'Bobby Brown',
+  email: 'bobby.brown@nav.no',
+  teams: ['nada', 'knada'],
 }
 
 export const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    req.cookies["login"] !== "innlogget" ? res.status(200).json(null): res.status(200).json(response)
+  req.cookies['login'] !== 'innlogget'
+    ? res.status(200).json(null)
+    : res.status(200).json(response)
 }
 
 export default handler
