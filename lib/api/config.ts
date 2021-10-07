2a324b65c99ae24529e2ffc174a5343ec3811a79
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-07 11:31:17 +0200

    Implement proxying for API calls

--- lib/api/config.ts
+++
@@ -1,9 +0,0 @@
-export const getBackendURI = (): string => {
-  if (
-    process?.env?.NEXT_PUBLIC_BACKEND &&
-    process.env.NEXT_PUBLIC_BACKEND.length
-  ) {
-    return process.env.NEXT_PUBLIC_BACKEND
-  }
-  return 'http://localhost:3000/api'
-}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { getBackendURI } = require('./lib/api/config.js')
+
 module.exports = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: `${getBackendURI()}/:path*`, // Proxy to backend
+    },
+  ],
 }
