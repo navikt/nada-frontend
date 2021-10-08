612abcbbd3737f031820b53d3e5590962fbf769e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-08 14:39:10 +0200

    remove unused imports

--- components/dataproducts/detail.tsx
+++ components/dataproducts/detail.tsx
@@ -2,10 +2,6 @@ import { DataproductSchema } from '../../lib/schema/schema_types'
 import { format, parseISO } from 'date-fns'
 import { nb } from 'date-fns/locale'
 import ReactMarkdown from 'react-markdown'
-import { NewDatasetForm } from '../forms/dataset/new'
-import apiPOST from '../../lib/api/post'
-import { useState } from 'react'
-import { Button } from '@navikt/ds-react'
 import DatasetList from './DatasetList'
 
 export interface DataproductDetailProps {
