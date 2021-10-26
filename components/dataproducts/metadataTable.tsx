0a7fa631c0eaf874328eb93987431791db9f84e6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-26 17:12:43 +0200

    fix: malformed DOM

--- components/dataproducts/metadataTable.tsx
+++ components/dataproducts/metadataTable.tsx
@@ -32,62 +32,66 @@ const gcpUrl = 'https://console.cloud.google.com'
 
 export const MetadataTable = ({ product }: DataproductDetailProps) => (
   <StyledMetadataTable>
-    <tr>
-      <th>Type:</th>
-      <td>{product.datasource?.__typename || 'who knows right'}</td>
-    </tr>
-    <tr>
-      <th>Team:</th>
-      <td>
-        {product.owner?.teamkatalogen ? (
-          <Link href={product.owner.teamkatalogen}>{product.owner.group}</Link>
-        ) : (
-          product.owner?.group
-        )}
-      </td>
-    </tr>
-    <tr>
-      <th>Adresse:</th>
-      <td>
-        <Link
-          href={`${gcpUrl}/bigquery?d=${product.datasource.dataset}&t=${product.datasource.table}&p=${product.datasource.projectID}&page=table`}
-        >
-          {`${product.datasource.projectID}.${product.datasource.dataset}.${product.datasource.table}`}
-        </Link>
-      </td>
-    </tr>
-    <tr>
-      <th>Nøkkelord:</th>
-      <td>
-        {product.keywords &&
-          product.keywords.map((k) => <Keyword key={k} keyword={k} />)}
-      </td>
-    </tr>
-    <tr>
-      <th>
-        <IconBox size={24} justifyRight>
-          <GithubIcon />
-        </IconBox>
-      </th>
-      <td>
-        <RepoKnapp url={product.repo} />
-      </td>
-    </tr>
-    <tr>
-      <th>
-        <IconBox size={24} justifyRight>
-          {product.pii ? (
-            <Warning style={{ fontSize: '1.5rem' }} color={navRod} />
+    <tbody>
+      <tr>
+        <th>Type:</th>
+        <td>{product.datasource?.__typename || 'who knows right'}</td>
+      </tr>
+      <tr>
+        <th>Team:</th>
+        <td>
+          {product.owner?.teamkatalogen ? (
+            <Link href={product.owner.teamkatalogen}>
+              {product.owner.group}
+            </Link>
           ) : (
-            <Success style={{ fontSize: '1.5rem' }} color={navGronn} />
+            product.owner?.group
           )}
-        </IconBox>
-      </th>
-      <td>
-        {' '}
-        Dette dataproduktet inneholder {!product.pii && <b> IKKE </b>}
-        personidentifiserende informasjon
-      </td>
-    </tr>
+        </td>
+      </tr>
+      <tr>
+        <th>Adresse:</th>
+        <td>
+          <Link
+            href={`${gcpUrl}/bigquery?d=${product.datasource.dataset}&t=${product.datasource.table}&p=${product.datasource.projectID}&page=table`}
+          >
+            {`${product.datasource.projectID}.${product.datasource.dataset}.${product.datasource.table}`}
+          </Link>
+        </td>
+      </tr>
+      <tr>
+        <th>Nøkkelord:</th>
+        <td>
+          {product.keywords &&
+            product.keywords.map((k) => <Keyword key={k} keyword={k} />)}
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <IconBox size={24} justifyRight>
+            <GithubIcon />
+          </IconBox>
+        </th>
+        <td>
+          <RepoKnapp url={product.repo} />
+        </td>
+      </tr>
+      <tr>
+        <th>
+          <IconBox size={24} justifyRight>
+            {product.pii ? (
+              <Warning style={{ fontSize: '1.5rem' }} color={navRod} />
+            ) : (
+              <Success style={{ fontSize: '1.5rem' }} color={navGronn} />
+            )}
+          </IconBox>
+        </th>
+        <td>
+          {' '}
+          Dette dataproduktet inneholder {!product.pii && <b> IKKE </b>}
+          personidentifiserende informasjon
+        </td>
+      </tr>
+    </tbody>
   </StyledMetadataTable>
 )
