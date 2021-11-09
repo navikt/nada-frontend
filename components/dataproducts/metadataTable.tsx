78b325c9a49125607e62c107721f89f95865b240
cteig <christine.teig@nav.no>
2021-11-09 20:39:08 +0100

    FIX: Show collections the products belongs to

--- components/dataproducts/metadataTable.tsx
+++ components/dataproducts/metadataTable.tsx
@@ -3,22 +3,49 @@ import Link from 'next/link'
 import StyledMetadataTable from '../lib/styledMetadataTable'
 import humanizeDate from '../lib/humanizeDate'
 import { DataproductQuery } from '../../lib/schema/graphql'
+import styled from 'styled-components'
 
 interface CollectionProps {
   collections: DataproductQuery['dataproduct']['collections']
 }
 
+const StyledLinks = styled.span`
+  ul {
+    display: inline;
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  ul li {
+    display: inline;
+  }
+
+  ul li:after {
+    content: ', ';
+  }
+
+  ul li:last-child:after {
+    content: '';
+  }
+`
+
 const CollectionLinks = ({ collections }: CollectionProps) => {
-  return collections.map((c, index) => {
-    return (
-      <>
-        <Link key={c.id} href={`/collection/${c.id}`} passHref>
-          {c.name}
-        </Link>
-        {index < collections.length - 1 ? ', ' : ''}
-      </>
-    )
-  })
+  return (
+    <StyledLinks>
+      <ul>
+        {collections.map((c) => {
+          return (
+            <li key={c.id}>
+              <Link href={`/collection/${c.id}`} passHref>
+                {c.name}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </StyledLinks>
+  )
 }
 
 export const MetadataTable = ({ product }: DataproductDetailProps) => (
@@ -42,7 +69,7 @@ export const MetadataTable = ({ product }: DataproductDetailProps) => (
       </tr>
       <tr>
         <th>Samling(er):</th>
-        <td key={product.id}>{CollectionLinks(product)}</td>
+        <td>{CollectionLinks(product)}</td>
       </tr>
     </tbody>
   </StyledMetadataTable>
