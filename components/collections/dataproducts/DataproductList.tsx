a01ababf346aa71acfb18683d204dea733891542
Johnny Horvi <johnny@horvi.no>
2021-12-01 10:52:22 +0100

    wip: use mui

--- components/collections/dataproducts/DataproductList.tsx
+++ components/collections/dataproducts/DataproductList.tsx
@@ -1,8 +1,18 @@
 import DataproductCard from './DataproductCard'
-import { CollectionQuery } from '../../../lib/schema/graphql'
+import { CollectionQuery, Group } from '../../../lib/schema/graphql'
 import styled from 'styled-components'
 import amplitudeLog from '../../../lib/amplitude'
 import Link from 'next/link'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Avatar from '@mui/material/Avatar'
+import BigQueryLogo from '../../lib/icons/bigQueryLogo'
+import { useContext } from 'react'
+import { UserState } from '../../../lib/context'
 
 interface DataproductListProps {
   collection: CollectionQuery['collection']
@@ -13,6 +23,12 @@ const Container = styled.div`
 `
 
 export const DataproductList = ({ collection }: DataproductListProps) => {
+  const userState = useContext(UserState)
+  const isOwner =
+    userState?.groups.some((g: Group) => {
+      return g.email === collection?.owner.group
+    }) || false
+
   return (
     <Container
       onClick={() => {
@@ -20,13 +36,30 @@ export const DataproductList = ({ collection }: DataproductListProps) => {
           fra: `collection-${collection.name}`,
         }
         amplitudeLog('navigere', eventProperties)
-      }}
-    >
-      {collection?.elements.map((d) => (
-        <Link key={d.id} href={`/dataproduct/${d.id}`} passHref>
-          <DataproductCard id={d.id} />
-        </Link>
-      ))}
+      }}>
+      <List>
+        {collection?.elements.map((d) => (
+          <Link href={`/dataproduct/${d.id}`} key={d.id}>
+            <ListItem
+              secondaryAction={
+                isOwner &&
+                <IconButton edge='end' aria-label='delete'>
+                  <DeleteIcon />
+                </IconButton>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar>
+                  <BigQueryLogo />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={d.name}
+              />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
     </Container>
   )
 }
