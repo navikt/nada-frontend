9f2fc57c187769f19e71f1e6ff7611fc09cd0b87
Johnny Horvi <johnny@horvi.no>
2021-12-02 08:10:27 +0100

    clean up user

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,20 +1,11 @@
-import styled from 'styled-components'
 import * as React from 'react'
 import { useContext, useState } from 'react'
 import { UserState } from '../lib/context'
 import TopBar from '../components/lib/topBar'
 import { Name } from '../components/lib/detailTypography'
 import { Tab, Tabs } from '@mui/material'
-import TabPanel from '../components/lib/tabPanel'
 import { MetadataTable } from '../components/user/metadataTable'
 import Head from 'next/head'
-
-const StyledTabPanel = styled(TabPanel)`
-  > div {
-    padding-left: 0px;
-    padding-right: 0px;
-  }
-`
 
 export const UserProductLink = () => {
   const userState = useContext(UserState)
@@ -43,12 +34,12 @@ export const UserProductLink = () => {
       <Tabs
         value={activeTab}
         onChange={handleChange}
-        variant="standard"
-        scrollButtons="auto"
-        aria-label="auto tabs example"
+        variant='standard'
+        scrollButtons='auto'
+        aria-label='auto tabs example'
       >
-        <Tab label="Mine produkter og samlinger" value={0} />
-        <Tab label="Mine tilganger" value={1} />
+        <Tab label='Mine produkter og samlinger' value={0} />
+        <Tab label='Mine tilganger' value={1} />
       </Tabs>
     </div>
   )
