07ed0c76dd9c8a5077a3f933f03b9a6fe48023e5
Johnny Horvi <johnny@horvi.no>
2021-12-10 16:24:34 +0100

    wip

--- components/dataproducts/access/owner.tsx
+++ components/dataproducts/access/owner.tsx
@@ -1,122 +1,263 @@
-import {QueryResult} from "@apollo/client";
-import {DataproductAccessQuery, Exact} from "../../../lib/schema/graphql";
-import ErrorMessage from "../../lib/error";
-import LoaderSpinner from "../../lib/spinner";
-import * as React from "react";
-import {useState} from "react";
-import {Button, Modal, TextField} from "@navikt/ds-react";
-import {useForm, Controller} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup/dist/yup";
-import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup} from "@mui/material";
-import RightJustifiedSubmitButton from "../../widgets/formSubmit";
-import * as yup from "yup";
+import { QueryResult } from '@apollo/client'
+import {
+  DataproductAccessQuery,
+  Exact, GrantAccessMutationVariables,
+  SubjectType,
+  useAddRequesterMutation,
+  useGrantAccessMutation,
+} from '../../../lib/schema/graphql'
+import ErrorMessage from '../../lib/error'
+import LoaderSpinner from '../../lib/spinner'
+import * as React from 'react'
+import { useState } from 'react'
+import { Button } from '@navikt/ds-react'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
+import { Box, FormControl, FormControlLabel, Modal, Radio, RadioGroup } from '@mui/material'
+import { TextField } from '@mui/material/'
+import RightJustifiedSubmitButton from '../../widgets/formSubmit'
+import * as yup from 'yup'
+import { DesktopDatePicker, LocalizationProvider } from '@mui/lab'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { endOfDay } from 'date-fns'
 
 export const addAccessValidation = yup.object().shape({
-    subject: yup
-        .string()
-        .email('Gyldig epost for gruppe eller bruker').required("Legg inn gyldig epost"),
-    subjectType: yup.string().required(),
-    expires: yup.date(),
+  subject: yup
+    .string()
+    .email('Gyldig epost for gruppe eller bruker').required('Legg inn gyldig epost'),
+  subjectType: yup.string().required(),
+  accessType: yup.string().required(),
+  expires: yup.string(),
 })
 
 interface OwnerProps {
-    accessQuery: QueryResult<DataproductAccessQuery, Exact<{ id: string }>>
+  accessQuery: QueryResult<DataproductAccessQuery, Exact<{ id: string }>>
 }
 
-const Owner = ({accessQuery}: OwnerProps) => {
-    const {error, loading, data: {dataproduct} = {}} = accessQuery
-    const [open, setOpen] = useState(false)
+const Owner = ({ accessQuery }: OwnerProps) => {
+  const { error, loading, data: { dataproduct } = {} } = accessQuery
+  const [open, setOpen] = useState(false)
+  const [formError, setFormError] = useState('')
 
+  const [date, setDate] = useState<Date | null>(endOfDay(new Date()))
+  const dateChange = (newValue: Date | null) => {
+    console.log('dateChange', newValue)
+    setDate(newValue)
+  }
+  const defaultValues = {
+    subjectType: '',
+    subject: '',
+    accessType: '',
+    expires: '',
+  }
+  const { formState, handleSubmit, control, watch, register, reset } =
+    useForm({
+      resolver: yupResolver(addAccessValidation),
+      defaultValues,
+    })
 
-    const {formState, handleSubmit, control, watch, register} =
-        useForm({
-            resolver: yupResolver(addAccessValidation),
-            defaultValues: {
-                subjectType: "",
-                subject: ""
-            }
+  const errors = formState.errors
+  console.log(errors)
+
+  const subjectType = watch('subjectType')
+  const subject = watch('subject')
+  const accessType = watch('accessType')
+
+  const [addRequester] = useAddRequesterMutation()
+  const [grantAccess] = useGrantAccessMutation()
+
+  if (error) return <ErrorMessage error={error} />
+  if (loading || !dataproduct) return <LoaderSpinner />
+
+  const onSubmit = async (requestData: { subjectType: string, subject: string, accessType: string, expires: any }) => {
+    requestData.expires = date
+    console.log(requestData)
+    let subjectWithType = `${requestData.subjectType}:${requestData.subject}`
+    if (requestData.subjectType === 'all-users') {
+      subjectWithType = 'group:all-users@nav.no'
+    }
+
+    if (requestData.accessType === 'ondemand') {
+      try {
+        await addRequester({
+          variables: {
+            dataproductID: dataproduct.id,
+            subject: subjectWithType,
+          },
+          refetchQueries: ['DataproductAccess'],
         })
-    const errors = formState.errors
-    console.log("###",errors)
+      } catch (e: any) {
+        setFormError(e.message)
+        return
+      }
+    }
+    try {
+      const variables: GrantAccessMutationVariables = {
+        subjectType: (SubjectType as any)[requestData.subjectType],
+        subject: requestData.subject,
+        dataproductID: dataproduct.id,
+      }
 
-    const subjectType = watch('subjectType')
+      if (requestData.accessType === 'until') {
+        variables.expires = date
+      }
 
-    if (error) return <ErrorMessage error={error}/>
-    if (loading || !dataproduct) return <LoaderSpinner/>
-
-    const onSubmit = (requestData: any) => {
-        console.log('pushed')
-        console.log(requestData)
+      await grantAccess({
+        variables,
+      })
+    } catch (e) {
+      setFormError(e.message)
+      return
     }
 
 
-    return (
-        <>
-            <Button key="legg til" variant="primary" size="small" onClick={() => setOpen(true)}>
-                legg til
-            </Button>
-            <br/>
-            {
-                (dataproduct.access.length === 0 && dataproduct.requesters.length === 0) && <>Ingen har tilgang til
-                    produktet</>
-            }
+    closeAndReset()
+  }
 
-            <Modal open={open} onClose={() => setOpen(false)}>
-                <Modal.Content>
-                    <div>
-                        <h1>
-                            Legg til tilgang
-                        </h1>
-                        <h3>Hvem skal ha tilgang?</h3>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <FormControl component="fieldset">
-                                <FormLabel component="legend">Brukertype</FormLabel>
-                                <Controller
-                                    rules={{ required: true }}
-                                    control={control}
-                                    name="subjectType"
-                                    render={({ field }) => (
-                                        <RadioGroup {...field}>
-                                            <FormControlLabel
-                                                value="all-users"
-                                                control={<Radio />}
-                                                label="alle i nav"
-                                            />
-                                            <FormControlLabel
-                                                value="gruppe"
-                                                control={<Radio />}
-                                                label="gruppe"
-                                            />
-                                            <FormControlLabel
-                                                value="bruker"
-                                                control={<Radio />}
-                                                label="bruker"
-                                            />
-                                            <FormControlLabel
-                                                value="serviceAccount"
-                                                control={<Radio />}
-                                                label="serviceAccount"
-                                            />
-                                        </RadioGroup>
-                                    )}
-                                />
-                            </FormControl>
-                            {!subjectType ? null: subjectType === 'all-users' ? <>alle brukere valgt</> :
-                                <TextField
-                                    style={{ width: '450px', display: 'block' }}
-                                    id="subject"
-                                    label={`navn på ${subjectType}`}
-                                    {...register('subject')}
-                                    error={errors.subject?.message}
-                                />
-                            }
-                            <RightJustifiedSubmitButton/>
-                        </form>
-                    </div>
-                </Modal.Content>
-            </Modal>
-        </>
-    )
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 550,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
+
+  const closeAndReset = () => {
+    console.log('CLOSE AND RESET')
+    reset(defaultValues)
+    setFormError('')
+    setOpen(false)
+  }
+
+  const subjectTypeMap = new Map<string, string>([
+    ['group', 'gruppe'],
+    ['user', 'bruker'],
+    ['serviceAccount', 'servicebruker'],
+  ])
+
+  return (
+    <>
+      <Button key='legg til' variant='primary' size='small' onClick={() => setOpen(true)}>
+        legg til
+      </Button>
+      <br />
+      {
+        (dataproduct.access.length === 0 && dataproduct.requesters.length === 0) ?
+          <>Ingen har tilgang til produktet</> :
+          <>{dataproduct.requesters}</>
+      }
+
+      <Modal open={open} onClose={closeAndReset}>
+        <Box sx={style}>
+          <div>
+            <h1>
+              Legg til tilgang
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+              <h3> Hvem skal ha tilgang? </h3>
+              <FormControl component='fieldset'>
+                <Controller
+                  rules={{ required: true }}
+                  control={control}
+                  name='subjectType'
+                  render={({ field }) => (
+                    <RadioGroup {...field}>
+                      <FormControlLabel
+                        value='all-users'
+                        control={<Radio />}
+                        label='Alle i NAV'
+                      />
+                      <FormControlLabel
+                        value='group'
+                        control={<Radio />}
+                        label='Gruppe'
+                      />
+                      <FormControlLabel
+                        value='user'
+                        control={<Radio />}
+                        label='Bruker'
+                      />
+                      <FormControlLabel
+                        value='serviceAccount'
+                        control={<Radio />}
+                        label='Servicebruker'
+                      />
+                    </RadioGroup>
+                  )}
+                />
+              </FormControl>
+              {subjectType && subjectType !== 'all-users' &&
+              <>
+                <hr />
+                <h3>{`Navn på ${subjectTypeMap.get(subjectType)}`}</h3>
+                <TextField
+                  style={{ width: '450px', display: 'block' }}
+                  id='subject'
+                  error={typeof errors?.subject !== 'undefined'}
+                  helperText={errors?.subject?.message}
+                  {...register('subject')}
+                />
+                <br />
+              </>
+              }
+              {subject &&
+              <>
+                <hr />
+                <h3> Hvem skal ha tilgang? </h3>
+                <FormControl component='fieldset'>
+                  <Controller
+                    rules={{ required: true }}
+                    control={control}
+                    name='accessType'
+                    render={({ field }) => (
+                      <RadioGroup {...field}>
+                        <FormControlLabel
+                          value='ondemand'
+                          control={<Radio />}
+                          label='Så lenge de ber om det'
+                        />
+                        <FormControlLabel
+                          value='eternal'
+                          control={<Radio />}
+                          label='Evig'
+                        />
+                        <FormControlLabel
+                          value='until'
+                          control={<Radio />}
+                          label='Til denne datoen'
+                        />
+                      </RadioGroup>
+                    )}
+                  />
+                </FormControl>
+              </>
+              }
+              {accessType && accessType === 'until' &&
+              <>
+                <br />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DesktopDatePicker
+                    inputFormat='MM/dd/yyyy'
+                    value={date}
+                    onChange={dateChange}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </>
+              }
+              <div>{formError}</div>
+              <RightJustifiedSubmitButton onCancel={closeAndReset} />
+            </form>
+          </div>
+        </Box>
+      </Modal>
+    </>
+  )
 }
 
 
--- pages/dataproduct/[id]/[[...page]].tsx
+++ pages/dataproduct/[id]/[[...page]].tsx
@@ -1,32 +1,32 @@
 import LoaderSpinner from '../../../components/lib/spinner'
 import ErrorMessage from '../../../components/lib/error'
 import {
-    Group,
-    useDataproductAccessQuery,
-    useDataproductQuery,
-    useUserInfoDetailsQuery
+  Group,
+  useDataproductAccessQuery,
+  useDataproductQuery,
+  useUserInfoDetailsQuery,
 } from '../../../lib/schema/graphql'
-import {GetServerSideProps} from 'next'
-import {addApolloState, initializeApollo} from '../../../lib/apollo'
-import {GET_DATAPRODUCT} from '../../../lib/queries/dataproduct/dataproduct'
+import { GetServerSideProps } from 'next'
+import { addApolloState, initializeApollo } from '../../../lib/apollo'
+import { GET_DATAPRODUCT } from '../../../lib/queries/dataproduct/dataproduct'
 import * as React from 'react'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import amplitudeLog from '../../../lib/amplitude'
 import Head from 'next/head'
-import {BackButton} from "../../../components/lib/BackButton";
-import TopBar from "../../../components/lib/topBar";
-import {Name} from "../../../components/lib/detailTypography";
-import EditMenu from "../../../components/lib/editMenu";
-import {MetadataTable} from "../../../components/dataproducts/metadataTable";
-import styled from "styled-components";
-import {useRouter} from 'next/router'
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import TabPanel, {TabPanelType} from "../../../components/lib/tabPanel";
-import DataproductInfo from "../../../components/dataproducts/dataproductInfo";
-import DataproductTableSchema from "../../../components/dataproducts/dataproductTableSchema";
-import Owner from "../../../components/dataproducts/access/owner";
-import {GET_DATAPRODUCT_ACCESS} from "../../../lib/queries/access/dataproductAccess";
+import { BackButton } from '../../../components/lib/BackButton'
+import TopBar from '../../../components/lib/topBar'
+import { Name } from '../../../components/lib/detailTypography'
+import EditMenu from '../../../components/lib/editMenu'
+import { MetadataTable } from '../../../components/dataproducts/metadataTable'
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import TabPanel, { TabPanelType } from '../../../components/lib/tabPanel'
+import DataproductInfo from '../../../components/dataproducts/dataproductInfo'
+import DataproductTableSchema from '../../../components/dataproducts/dataproductTableSchema'
+import Owner from '../../../components/dataproducts/access/owner'
+import { GET_DATAPRODUCT_ACCESS } from '../../../lib/queries/access/dataproductAccess'
 
 
 const Container = styled.div`
@@ -39,156 +39,162 @@ const Product = styled.div`
 `
 
 interface DataproductProps {
-    id: string
+  id: string
 }
 
 const Dataproduct = (props: DataproductProps) => {
-    const {id} = props
-    const router = useRouter()
+  const { id } = props
+  const router = useRouter()
 
-    const [showDelete, setShowDelete] = useState(false)
+  const [showDelete, setShowDelete] = useState(false)
 
-    const userInfo = useUserInfoDetailsQuery().data?.userInfo
-    const productQuery = useDataproductQuery({
-        variables: {id},
-        ssr: true
-    })
+  const userInfo = useUserInfoDetailsQuery().data?.userInfo
+  const productQuery = useDataproductQuery({
+    variables: { id },
+    ssr: true,
+  })
 
-    const accessQuery = useDataproductAccessQuery({
-        variables: {id},
-        ssr: true
-    })
+  const accessQuery = useDataproductAccessQuery({
+    variables: { id },
+    ssr: true,
+  })
 
-    useEffect(() => {
-        const eventProperties = {
-            sidetittel: 'produktside',
-            title: productQuery.data?.dataproduct.name,
-        }
-        amplitudeLog('sidevisning', eventProperties)
-    })
-
-    if (productQuery.error) return <ErrorMessage error={productQuery.error}/>
-    if (productQuery.loading || !productQuery.data?.dataproduct) return <LoaderSpinner/>
-
-    const product = productQuery.data.dataproduct
-
-    const isOwner =
-        userInfo?.groups.some((g: Group) => {
-            return g.email === product.owner.group
-        }) || false
-
-
-    const menuItems: Array<{
-        title: string
-        slug: string
-        component: any
-    }> = [
-        {
-            title: 'Informasjon',
-            slug: 'info',
-            component: (
-                <DataproductInfo product={product}/>
-            ),
-        },
-        {
-            title: 'Skjema',
-            slug: 'schema',
-            component: (
-                <DataproductTableSchema datasource={product.datasource}/>
-            ),
-        },
-        {
-            title: 'tilganger',
-            slug: 'access',
-            component: !userInfo ? <>Du må logge inn for å gjøre noe her</> : userInfo && isOwner ?
-                <Owner accessQuery={accessQuery}/> : <>user panel</>
-        },
-    ]
-
-
-    const currentPage = menuItems
-        .map((e) => e.slug)
-        .indexOf(router.query.page?.[0] ?? 'info')
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        router.push(`/dataproduct/${id}/${menuItems[newValue].slug}`)
+  useEffect(() => {
+    const eventProperties = {
+      sidetittel: 'produktside',
+      title: productQuery.data?.dataproduct.name,
     }
+    amplitudeLog('sidevisning', eventProperties)
+  })
+
+  if (productQuery.error) return <ErrorMessage error={productQuery.error} />
+  if (productQuery.loading || !productQuery.data?.dataproduct) return <LoaderSpinner />
+
+  const product = productQuery.data.dataproduct
+
+  const isOwner =
+    userInfo?.groups.some((g: Group) => {
+      return g.email === product.owner.group
+    }) || false
 
 
-    return (
-        <>
-            <Head>
-                <title>{product.name}</title>
-            </Head>
-            <Container>
-                <BackButton/>
-                <Product>
-                    <TopBar type={'Dataproduct'}>
-                        <Name>{product.name}</Name>
-                        {isOwner && (
-                            <EditMenu
-                                onEdit={() => router.push(`/dataproduct/${product.id}/edit`)}
-                                onDelete={() => setShowDelete(true)}
-                            />
-                        )}
-                    </TopBar>
-                    <MetadataTable product={product}/>
-                    <Tabs
-                        variant='standard'
-                        value={currentPage}
-                        onChange={handleChange}
-                        aria-label='dataprodukt-tabs'
-                    >
-                        {menuItems.map((i, idx) => (
-                            <Tab
-                                key={idx}
-                                label={i.title}
-                            />
-                        ))}
-                    </Tabs>
-                    {menuItems.map((i, idx) => (
-                        <TabPanel
-                            key={idx}
-                            value={currentPage}
-                            index={idx}
-                            type={TabPanelType.simple}
-                        >
-                            {i.component}
-                        </TabPanel>
-                    ))}
-                </Product>
-            </Container>
-        </>
-    )
+  const menuItems: Array<{
+    title: string
+    slug: string
+    component: any
+  }> = [
+    {
+      title: 'Informasjon',
+      slug: 'info',
+      component: (
+        <DataproductInfo product={product} />
+      ),
+    },
+    {
+      title: 'Skjema',
+      slug: 'schema',
+      component: (
+        <DataproductTableSchema datasource={product.datasource} />
+      ),
+    },
+    {
+      title: 'tilganger',
+      slug: 'access',
+      component: !userInfo ? <>Du må logge inn for å gjøre noe her</> : userInfo && isOwner ?
+        <Owner accessQuery={accessQuery} /> : <>user panel</>,
+    },
+  ]
+
+
+  const currentPage = menuItems
+    .map((e) => e.slug)
+    .indexOf(router.query.page?.[0] ?? 'info')
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    router.push(`/dataproduct/${id}/${menuItems[newValue].slug}`)
+  }
+
+
+  return (
+    <>
+      <Head>
+        <title>{product.name}</title>
+      </Head>
+      <Container>
+        <BackButton />
+        <Product>
+          <TopBar type={'Dataproduct'}>
+            <Name>{product.name}</Name>
+            {isOwner && (
+              <EditMenu
+                onEdit={() => router.push(`/dataproduct/${product.id}/edit`)}
+                onDelete={() => setShowDelete(true)}
+              />
+            )}
+          </TopBar>
+          <MetadataTable product={product} />
+          <Tabs
+            variant='standard'
+            value={currentPage}
+            onChange={handleChange}
+            aria-label='dataprodukt-tabs'
+          >
+            {menuItems.map((i, idx) => (
+              <Tab
+                key={idx}
+                label={i.title}
+              />
+            ))}
+          </Tabs>
+          {menuItems.map((i, idx) => (
+            <TabPanel
+              key={idx}
+              value={currentPage}
+              index={idx}
+              type={TabPanelType.simple}
+            >
+              {i.component}
+            </TabPanel>
+          ))}
+        </Product>
+      </Container>
+    </>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const {id} = context.query
-    const cookie = context.req.headers.cookie
+  const { id } = context.query
+  const cookie = context.req.headers.cookie
 
-    const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo()
 
-    await apolloClient.query({
-        query: GET_DATAPRODUCT,
-        variables: {id},
-        context: {
-            headers: {
-                cookie,
-            },
-        },
-    })
-    await apolloClient.query({
-        query: GET_DATAPRODUCT_ACCESS,
-        variables: {id},
-        context: {
-            headers: {
-                cookie,
-            },
-        },
-    })
-    return addApolloState(apolloClient, {
-        props: {id},
-    })
+  await apolloClient.query({
+    query: GET_DATAPRODUCT,
+    variables: { id },
+    context: {
+      headers: {
+        cookie,
+      },
+    },
+  })
+
+  try {
+      await apolloClient.query({
+          query: GET_DATAPRODUCT_ACCESS,
+          variables: {id},
+          context: {
+              headers: {
+                  cookie,
+              },
+          },
+      })
+  } catch (e) {
+      // ignore access denied if not logged in
+  }
+
+  return addApolloState(apolloClient, {
+    props: { id },
+  })
 }
 
 export default Dataproduct
