fd723791f0cfd2a7da01f1686c75ea996a23d7d8
Johnny Horvi <johnny@horvi.no>
2021-12-14 10:36:14 +0100

    wip

--- components/dataproducts/access/accessList.tsx
+++ components/dataproducts/access/accessList.tsx
@@ -1,89 +1,91 @@
-import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-import * as React from "react";
-import {Delete, Success, Error} from "@navikt/ds-icons";
-import {navGronn, navRod} from "../../../styles/constants";
-import humanizeDate from "../../lib/humanizeDate";
-import { parseISO, isAfter } from "date-fns";
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
+import * as React from 'react'
+import { Delete, Error, Success } from '@navikt/ds-icons'
+import { navGronn, navRod } from '../../../styles/constants'
+import humanizeDate from '../../lib/humanizeDate'
+import { isAfter, parseISO } from 'date-fns'
 
 interface AccessEntry {
-    subject: string,
-    canRequest: boolean,
-    access?: access,
+  subject: string,
+  canRequest: boolean,
+  access?: access,
 
 }
 
 const productAccess = (access: access[], requesters: string[]): AccessEntry[] => {
-    // Build accessEntries based on requesters
-    const ret = requesters.map((r): AccessEntry => {
-        return {subject: r, canRequest: true}
-    })
+  // Build accessEntries based on requesters
+  const ret: AccessEntry[] = requesters.map((r): AccessEntry => {
+    return { subject: r, canRequest: true }
+  })
 
-    // Add access to existing ret entries or create a new entry.
-    access.filter(a => !a.revoked || isAfter(parseISO(a.expires), Date.now())).forEach(a => {
-        ret.forEach((entry, i) => {
-            const accessSubject = a.subject.split(":")[1]
+  // Add access to existing ret entries or create a new entry.
+  const valid = (a: access) => !a.revoked || isAfter(parseISO(a.expires), Date.now())
 
-            if (entry.subject === accessSubject) {
-                ret[i].access = a
-            } else if (!ret.map(r => r.subject).includes(accessSubject)){
-                ret.push({subject: accessSubject, access: a, canRequest: false})
-            }
-        })
-    })
-    return ret
+  access.filter(valid).forEach(a => {
+    // check if we have a entry in ret with subject === a.subject,
+    // if so we enrich with a, else push new accessentry
+    const subject = a.subject.split(':')[1]
+    const i = ret.findIndex((e: AccessEntry) => e.subject === subject)
+    if (i === -1) { // not found
+      ret.push({ subject, access: a, canRequest: false })
+    } else {
+      ret[i].access = a
+    }
+  })
 
-
+  return ret
 }
 
 interface access {
-    id: string;
-    subject: string;
-    granter: string;
-    expires?: any;
-    created: any;
-    revoked?: any;
+  id: string;
+  subject: string;
+  granter: string;
+  expires?: any;
+  created: any;
+  revoked?: any;
 }
 
 function removeAccess(id: string, a: AccessEntry, requesters: string[]) {
-    console.log("Going to delete stuff")
+  console.log('Going to delete stuff')
 }
 
 interface AccessListProps {
-    id: string,
-    access: access[],
-    requesters: string[],
+  id: string,
+  access: access[],
+  requesters: string[],
 }
 
-const AccessList = ({id, access, requesters}: AccessListProps) => {
-    if (access.length === 0 && requesters.length === 0) {
-        return <>Ingen har tilgang til produktet</>
-    }
-    const accesses = productAccess(access, requesters)
+const AccessList = ({ id, access, requesters }: AccessListProps) => {
+  if (access.length === 0 && requesters.length === 0) {
+    return <>Ingen har tilgang til produktet</>
+  }
+  const accesses = productAccess(access, requesters)
 
-    return (
-        <Table sx={{minWidth: 650}} aria-label='simple table'>
-            <TableHead>
-                <TableRow>
-                    <TableCell align='left'>Bruker / gruppe</TableCell>
-                    <TableCell align='left'>Kan be om tilgang</TableCell>
-                    <TableCell align='left'>Har tilgang</TableCell>
-                    <TableCell align='left'>Fjern tilgang</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {accesses.map((a, i) => <TableRow key={i}>
-                    <TableCell>{a.subject}</TableCell>
-                    <TableCell align='center'>{a.canRequest ? <Success style={{color: navGronn}}/> :
-                        <Error style={{color: navRod}}/>}
-                    </TableCell>
-                    <TableCell>{a.access ? <>{a.access.expires ? humanizeDate(a.access.expires) : 'evig'}</> :
-                        <Error style={{color: navRod}}/>}
-                    </TableCell>
-                    <TableCell align='center'><Delete style={{cursor: 'pointer', color: navRod}} onClick={() => removeAccess(id, a, requesters)}/></TableCell>
-                </TableRow>)}
+  return (
+    <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <TableHead>
+        <TableRow>
+          <TableCell align='left'>Bruker / gruppe</TableCell>
+          <TableCell align='center'>Kan be om tilgang</TableCell>
+          <TableCell align='center'>Har tilgang</TableCell>
+          <TableCell align='center'>Fjern tilgang</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {accesses.map((a, i) => <TableRow key={i}>
+          <TableCell>{a.subject}</TableCell>
+          <TableCell align='center'>{a.canRequest ? <Success style={{ color: navGronn }} /> :
+            <Error style={{ color: navRod }} />}
+          </TableCell>
+          <TableCell align='center'>{a.access ? <>{a.access.expires ? humanizeDate(a.access.expires) : 'evig'}</> :
+            <Error style={{ color: navRod }} />}
+          </TableCell>
+          <TableCell align='center'><Delete style={{ cursor: 'pointer', color: navRod }}
+                                            onClick={() => removeAccess(id, a, requesters)} /></TableCell>
+        </TableRow>)}
 
-            </TableBody>
-        </Table>
-    )
+      </TableBody>
+    </Table>
+  )
 }
 export default AccessList
--- components/dataproducts/access/owner.tsx
+++ components/dataproducts/access/owner.tsx
@@ -1,272 +1,271 @@
-import {QueryResult} from '@apollo/client'
+import { QueryResult } from '@apollo/client'
 import {
-    DataproductAccessQuery,
-    Exact,
-    GrantAccessMutationVariables,
-    SubjectType,
-    useAddRequesterMutation,
-    useGrantAccessMutation,
+  DataproductAccessQuery,
+  Exact,
+  GrantAccessMutationVariables,
+  SubjectType,
+  useAddRequesterMutation,
+  useGrantAccessMutation,
 } from '../../../lib/schema/graphql'
 import ErrorMessage from '../../lib/error'
 import LoaderSpinner from '../../lib/spinner'
 import * as React from 'react'
-import {useState} from 'react'
-import {Alert, Button} from '@navikt/ds-react'
-import {Controller, useForm} from 'react-hook-form'
-import {yupResolver} from '@hookform/resolvers/yup/dist/yup'
-import {Box, FormControl, FormControlLabel, Modal, Radio, RadioGroup} from '@mui/material'
-import {TextField} from '@mui/material/'
+import { useState } from 'react'
+import { Alert, Button } from '@navikt/ds-react'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
+import { Box, FormControl, FormControlLabel, Modal, Radio, RadioGroup, TextField } from '@mui/material'
 import RightJustifiedSubmitButton from '../../widgets/formSubmit'
 import * as yup from 'yup'
-import {DesktopDatePicker, LocalizationProvider} from '@mui/lab'
+import { DesktopDatePicker, LocalizationProvider } from '@mui/lab'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import {endOfDay} from 'date-fns'
-import AccessList from "./accessList";
+import { endOfDay } from 'date-fns'
+import AccessList from './accessList'
 
 export const addAccessValidation = yup.object().shape({
-    subjectType: yup.string().required(),
-    subject: yup
-        .string()
-        .email('Gyldig epost for gruppe eller bruker').when("subjectType", {
-            is: (subjectType: string) => subjectType !== 'all-users',
-            then: yup.string().required('Legg inn gyldig epost')
-        }),
-    accessType: yup.string().required(),
-    expires: yup.string(),
+  subjectType: yup.string().required(),
+  subject: yup
+    .string()
+    .email('Gyldig epost for gruppe eller bruker').when('subjectType', {
+      is: (subjectType: string) => subjectType !== 'all-users',
+      then: yup.string().required('Legg inn gyldig epost'),
+    }),
+  accessType: yup.string().required(),
+  expires: yup.string(),
 })
 
 interface OwnerProps {
-    accessQuery: QueryResult<DataproductAccessQuery, Exact<{ id: string }>>
+  accessQuery: QueryResult<DataproductAccessQuery, Exact<{ id: string }>>
 }
 
-const Owner = ({accessQuery}: OwnerProps) => {
-    const {error, loading, data: {dataproduct} = {}} = accessQuery
-    const [open, setOpen] = useState(false)
-    const [formError, setFormError] = useState('')
+const Owner = ({ accessQuery }: OwnerProps) => {
+  const { error, loading, data: { dataproduct } = {} } = accessQuery
+  const [open, setOpen] = useState(false)
+  const [formError, setFormError] = useState('')
 
-    const [date, setDate] = useState<Date | null>(endOfDay(new Date()))
-    const dateChange = (newValue: Date | null) => {
-        setDate(newValue)
-    }
-    const defaultValues = {
-        subjectType: '',
-        subject: '',
-        accessType: '',
-        expires: '',
-    }
-    const {formState, handleSubmit, control, watch, register, reset} =
-        useForm({
-            resolver: yupResolver(addAccessValidation),
-            defaultValues,
+  const [date, setDate] = useState<Date | null>(endOfDay(new Date()))
+  const dateChange = (newValue: Date | null) => {
+    setDate(newValue)
+  }
+  const defaultValues = {
+    subjectType: '',
+    subject: '',
+    accessType: '',
+    expires: '',
+  }
+  const { formState, handleSubmit, control, watch, register, reset } =
+    useForm({
+      resolver: yupResolver(addAccessValidation),
+      defaultValues,
+    })
+
+  const errors = formState.errors
+
+  const subjectType = watch('subjectType')
+  const subject = watch('subject')
+  const accessType = watch('accessType')
+
+  const [addRequester] = useAddRequesterMutation()
+  const [grantAccess] = useGrantAccessMutation()
+
+  if (error) return <ErrorMessage error={error} />
+  if (loading || !dataproduct) return <LoaderSpinner />
+
+  const onSubmit = async (requestData: { subjectType: string, subject: string, accessType: string, expires: any }) => {
+    requestData.expires = date
+    const accessSubject = requestData.subjectType === 'all-users' ? 'all-users@nav.no' : subject
+
+    if (requestData.accessType === 'ondemand') {
+      try {
+        await addRequester({
+          variables: {
+            dataproductID: dataproduct.id,
+            subject: accessSubject,
+          },
+          refetchQueries: ['DataproductAccess'],
         })
-
-    const errors = formState.errors
-
-    const subjectType = watch('subjectType')
-    const subject = watch('subject')
-    const accessType = watch('accessType')
-
-    const [addRequester] = useAddRequesterMutation()
-    const [grantAccess] = useGrantAccessMutation()
-
-    if (error) return <ErrorMessage error={error}/>
-    if (loading || !dataproduct) return <LoaderSpinner/>
-
-    const onSubmit = async (requestData: { subjectType: string, subject: string, accessType: string, expires: any }) => {
-        requestData.expires = date
-        const accessSubject = requestData.subjectType === 'all-users' ? 'all-users@nav.no' : subject
-
-        if (requestData.accessType === 'ondemand') {
-            try {
-               await addRequester({
-                    variables: {
-                        dataproductID: dataproduct.id,
-                        subject: accessSubject,
-                    },
-                    refetchQueries: ['DataproductAccess'],
-                })
-               closeAndReset()
-                return
-            } catch (e: any) {
-                setFormError(e.message)
-                return
-            }
-        }
-
-        const toSubjectType = (s: string): SubjectType => {
-            switch (s) {
-                case "all-users" :
-                case "group" :
-                    return SubjectType.Group
-                case "serviceAccount" :
-                    return SubjectType.ServiceAccount
-            }
-            return SubjectType.User
-
-        }
-
-        try {
-            const variables: GrantAccessMutationVariables = {
-                subjectType: toSubjectType(requestData.subjectType),
-                subject: accessSubject,
-                dataproductID: dataproduct.id,
-            }
-
-            if (requestData.accessType === 'until') {
-                variables.expires = date
-            }
-
-            await grantAccess({
-                variables,
-                refetchQueries: ['DataproductAccess']
-            })
-            closeAndReset()
-            return
-        } catch (e: any) {
-            setFormError(e.message)
-            return
-        }
+        closeAndReset()
+        return
+      } catch (e: any) {
+        setFormError(e.message)
+        return
+      }
     }
 
-    const style = {
-        position: 'absolute' as 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 550,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
+    const toSubjectType = (s: string): SubjectType => {
+      switch (s) {
+        case 'all-users' :
+        case 'group' :
+          return SubjectType.Group
+        case 'serviceAccount' :
+          return SubjectType.ServiceAccount
+      }
+      return SubjectType.User
+
     }
 
-    const closeAndReset = () => {
-        reset(defaultValues)
-        setFormError('')
-        setOpen(false)
+    try {
+      const variables: GrantAccessMutationVariables = {
+        subjectType: toSubjectType(requestData.subjectType),
+        subject: accessSubject,
+        dataproductID: dataproduct.id,
+      }
+
+      if (requestData.accessType === 'until') {
+        variables.expires = date
+      }
+
+      await grantAccess({
+        variables,
+        refetchQueries: ['DataproductAccess'],
+      })
+      closeAndReset()
+      return
+    } catch (e: any) {
+      setFormError(e.message)
+      return
     }
+  }
 
-    const subjectTypeMap = new Map<string, string>([
-        ['group', 'gruppe'],
-        ['user', 'bruker'],
-        ['serviceAccount', 'servicebruker'],
-    ])
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 550,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
 
-    return (
-        <>
-            <Button key='legg til' variant='primary' size='small' onClick={() => setOpen(true)}>
-                legg til
-            </Button>
-            <br/>
-            <AccessList id={dataproduct.id} access={dataproduct.access} requesters={dataproduct.requesters}/>
-            <Modal open={open} onClose={closeAndReset}>
-                <Box sx={style}>
-                    <div>
-                        <h1>
-                            Legg til tilgang
-                        </h1>
-                        <form onSubmit={handleSubmit(onSubmit)}>
+  const closeAndReset = () => {
+    reset(defaultValues)
+    setFormError('')
+    setOpen(false)
+  }
 
-                            <h3> Hvem skal ha tilgang? </h3>
-                            <FormControl component='fieldset'>
-                                <Controller
-                                    rules={{required: true}}
-                                    control={control}
-                                    name='subjectType'
-                                    render={({field}) => (
-                                        <RadioGroup {...field}>
-                                            <FormControlLabel
-                                                value='all-users'
-                                                control={<Radio/>}
-                                                label='Alle i NAV'
-                                            />
-                                            <FormControlLabel
-                                                value='group'
-                                                control={<Radio/>}
-                                                label='Gruppe'
-                                            />
-                                            <FormControlLabel
-                                                value='user'
-                                                control={<Radio/>}
-                                                label='Bruker'
-                                            />
-                                            <FormControlLabel
-                                                value='serviceAccount'
-                                                control={<Radio/>}
-                                                label='Servicebruker'
-                                            />
-                                        </RadioGroup>
-                                    )}
-                                />
-                            </FormControl>
-                            {subjectType && subjectType !== 'all-users' &&
-                            <>
-                                <hr/>
-                                <h3>{`Navn på ${subjectTypeMap.get(subjectType)}`}</h3>
-                                <TextField
-                                    style={{width: '450px', display: 'block'}}
-                                    id='subject'
-                                    error={typeof errors?.subject !== 'undefined'}
-                                    helperText={errors?.subject?.message}
-                                    {...register('subject')}
-                                />
-                                <br/>
-                            </>
-                            }
-                            {(subject || subjectType === 'all-users') &&
-                            <>
-                                <hr/>
-                                <h3> Hva slags tilgang? </h3>
-                                <FormControl component='fieldset'>
-                                    <Controller
-                                        rules={{required: true}}
-                                        control={control}
-                                        name='accessType'
-                                        render={({field}) => (
-                                            <RadioGroup {...field}>
-                                                <FormControlLabel
-                                                    value='ondemand'
-                                                    control={<Radio/>}
-                                                    label='Må gi seg selv tilgang ved behov'
-                                                />
-                                                <FormControlLabel
-                                                    value='eternal'
-                                                    control={<Radio/>}
-                                                    label='Har alltid tilgang'
-                                                />
-                                                <FormControlLabel
-                                                    value='until'
-                                                    control={<Radio/>}
-                                                    label='Har tilgang til denne datoen'
-                                                />
-                                            </RadioGroup>
-                                        )}
-                                    />
-                                </FormControl>
-                            </>
-                            }
-                            {accessType && accessType === 'until' &&
-                            <>
-                                <br/>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <DesktopDatePicker
-                                        inputFormat='MM/dd/yyyy'
-                                        value={date}
-                                        onChange={dateChange}
-                                        renderInput={(params) => <TextField {...params} />}
-                                    />
-                                </LocalizationProvider>
-                            </>
-                            }
+  const subjectTypeMap = new Map<string, string>([
+    ['group', 'gruppe'],
+    ['user', 'bruker'],
+    ['serviceAccount', 'servicebruker'],
+  ])
 
-                            {formError && <Alert variant={'error'}>{formError}</Alert>}
-                            <RightJustifiedSubmitButton onCancel={closeAndReset}/>
-                        </form>
-                    </div>
-                </Box>
-            </Modal>
-        </>
-    )
+  return (
+    <>
+      <Button key='legg til' variant='primary' size='small' onClick={() => setOpen(true)}>
+        legg til
+      </Button>
+      <br />
+      <AccessList id={dataproduct.id} access={dataproduct.access} requesters={dataproduct.requesters} />
+      <Modal open={open} onClose={closeAndReset}>
+        <Box sx={style}>
+          <div>
+            <h1>
+              Legg til tilgang
+            </h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+              <h3> Hvem skal ha tilgang? </h3>
+              <FormControl component='fieldset'>
+                <Controller
+                  rules={{ required: true }}
+                  control={control}
+                  name='subjectType'
+                  render={({ field }) => (
+                    <RadioGroup {...field}>
+                      <FormControlLabel
+                        value='all-users'
+                        control={<Radio />}
+                        label='Alle i NAV'
+                      />
+                      <FormControlLabel
+                        value='group'
+                        control={<Radio />}
+                        label='Gruppe'
+                      />
+                      <FormControlLabel
+                        value='user'
+                        control={<Radio />}
+                        label='Bruker'
+                      />
+                      <FormControlLabel
+                        value='serviceAccount'
+                        control={<Radio />}
+                        label='Servicebruker'
+                      />
+                    </RadioGroup>
+                  )}
+                />
+              </FormControl>
+              {subjectType && subjectType !== 'all-users' &&
+              <div>
+                <hr />
+                <h3>{`Navn på ${subjectTypeMap.get(subjectType)}`}</h3>
+                <TextField
+                  sx={{ width: '300px' }}
+                  id='subject'
+                  error={typeof errors?.subject !== 'undefined'}
+                  helperText={errors?.subject?.message}
+                  {...register('subject')}
+                />
+                <br />
+              </div>
+              }
+              {(subject || subjectType === 'all-users') &&
+              <>
+                <hr />
+                <h3> Hva slags tilgang? </h3>
+                <FormControl component='fieldset'>
+                  <Controller
+                    rules={{ required: true }}
+                    control={control}
+                    name='accessType'
+                    render={({ field }) => (
+                      <RadioGroup {...field}>
+                        <FormControlLabel
+                          value='ondemand'
+                          control={<Radio />}
+                          label='Må gi seg selv tilgang ved behov'
+                        />
+                        <FormControlLabel
+                          value='eternal'
+                          control={<Radio />}
+                          label='Har alltid tilgang'
+                        />
+                        <FormControlLabel
+                          value='until'
+                          control={<Radio />}
+                          label='Har tilgang til denne datoen'
+                        />
+                      </RadioGroup>
+                    )}
+                  />
+                </FormControl>
+              </>
+              }
+              {accessType && accessType === 'until' &&
+              <>
+                <br />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DesktopDatePicker
+                    inputFormat='MM/dd/yyyy'
+                    value={date}
+                    onChange={dateChange}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </>
+              }
+
+              {formError && <Alert variant={'error'}>{formError}</Alert>}
+              <RightJustifiedSubmitButton onCancel={closeAndReset} />
+            </form>
+          </div>
+        </Box>
+      </Modal>
+    </>
+  )
 }
 
 
