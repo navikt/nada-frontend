05f5b87176b01afd2c292c654e3518660997aa1b
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2022-02-18 11:03:47 +0100

    Use the time element with humanizeDate
    The time element contains the datetime property with full time, as well
    as a title to show more details to the user.

---
+++ lib/humanizeDate.tsx
@@ -0,0 +1,13 @@
+import { format, parseISO } from 'date-fns'
+import { nb } from 'date-fns/locale'
+const humanizeDate = (isoDate: string, dateFormat = 'PPPP') => {
+  try {
+    const parsed = parseISO(isoDate);
+    return <time dateTime={isoDate} title={format(parsed, "dd. MMMM yyyy HH:mm:ii", { locale: nb })}>
+      {format(parsed, dateFormat, { locale: nb })}
+    </time>
+  } catch (e) {
+    return <></>
+  }
+}
+export default humanizeDate
