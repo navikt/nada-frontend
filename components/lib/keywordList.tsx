e3479a69565daa5de5679a4d2902b25309e43dce
Kent D <kent.daleng@nav.no>
2022-06-07 13:06:00 +0200

    edit keyword style to be more like designsystemet

--- components/lib/keywordList.tsx
+++ components/lib/keywordList.tsx
@@ -1,3 +1,4 @@
+import { Close } from "@navikt/ds-icons";
 import styled from "styled-components";
 import StringToColor from "../../lib/stringToColor";
 
@@ -14,17 +15,19 @@ interface KeywordSpanProps {
 
 const KeywordPillStyle = styled.span<KeywordSpanProps>`
   cursor: pointer;
-  display: ${(props) => props.horizontal ? 'block' : 'inline-block'};
+  display: ${(props) => props.horizontal ? 'flex' : 'inline-flex'};
+  align-items: center;
+  column-gap: 0.25rem;
   background-color: ${(props) => props.color};
   color: ${(props) => props.textColor};
-  border-radius: 999px;
+  border-radius: 4px;
   font-size: 12px;
   padding: 3px 5px 3px 5px;
+  margin-right: 0.25rem;
   border: 0.5px solid transparent;
   :hover {
     ${(props) => props.remove && 'text-decoration: line-through;'}
     ${(props) => props.remove && 'text-decoration-thickness: 3px;'}
-  
   }
 `
 
@@ -41,7 +44,7 @@ export const KeywordPill = ({keyword, horizontal, children, compact, onClick, re
     const color = StringToColor(keyword)
     return <KeywordPillStyle color={color} textColor={contrastColor({bgColor: color})} horizontal={horizontal}
                              compact={compact} onClick={onClick} remove={remove}>
-        {children}
+        {children}{remove && <Close />}
     </KeywordPillStyle>
 
 }
