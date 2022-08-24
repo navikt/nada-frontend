809fb21a8f048bf5a582f8fbd6983bd69268c6bc
Meng Zhu <meng.zhu@nav.no>
2022-08-24 11:53:21 +0200

    change keyword colors to match design

--- lib/stringToColor.ts
+++ lib/stringToColor.ts
@@ -1,14 +1,22 @@
+import { theme } from '@navikt/ds-tailwind'
+
+const keywordColors = [
+  'limegreen-100',
+  'green-100',
+  'orange-100',
+  'deepblue-100',
+  'purple-100',
+  'gray-100',
+]
+
 const StringToColor = (str: string) => {
   let hash = 0
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  //make sure same keyword get same color
+  let trimed = str.replace(/\s/g, '').toLocaleLowerCase()
+  for (let i = 0; i < trimed.length; i++) {
+    hash += trimed.charCodeAt(i)
   }
-  let colour = '#'
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff
-    colour += ('00' + value.toString(16)).substr(-2)
-  }
-  return colour
+  return theme.colors[keywordColors[hash % keywordColors.length]]
 }
 
 export default StringToColor
