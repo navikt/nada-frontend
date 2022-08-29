274eb57655bc1a3df1c3048e46e7afa319469b71
Erik Vattekar <erik.vattekar@nav.no>
2022-08-29 18:11:21 +0200

    fix: non-clickable keywords shall not look like links on hover

--- components/lib/detailTypography.tsx
+++ components/lib/detailTypography.tsx
@@ -3,32 +3,31 @@ import * as React from 'react'
 import remarkGfm from 'remark-gfm'
 import KeywordPill from './keywordList'
 import { Link } from '@navikt/ds-react'
+import { useRouter } from 'next/router'
 
 export const Description = ({
-  keywords,
-  markdown,
+    keywords,
+    markdown,
 }: {
-  keywords: string[]
-  markdown?: string | null
-}) => (
-  <div className="rounded-xl text-justify mt-8">
-    <ReactMarkdown remarkPlugins={[remarkGfm]}>
-      {markdown || '*ingen beskrivelse*'}
-    </ReactMarkdown>
-    {!!keywords.length && (
-      <div className="flex flex-row gap-1 flex-wrap my-2">
-        <>
-          {keywords.map((k, i) => (
-            <Link key={i} href={`/search?keywords=${k}`}>
-              <a>
-                <KeywordPill key={k} keyword={k}>
-                  {k}
-                </KeywordPill>
-              </a>
-            </Link>
-          ))}
-        </>
-      </div>
-    )}
-  </div>
-)
+    keywords: string[]
+    markdown?: string | null
+}) => {
+    const router = useRouter()
+
+    return (<div className="rounded-xl text-justify mt-8">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {markdown || '*ingen beskrivelse*'}
+        </ReactMarkdown>
+        {!!keywords.length && (
+            <div className="flex flex-row gap-1 flex-wrap my-2">
+                {keywords.map((k, i) => (
+                    <KeywordPill key={k} keyword={k} onClick={() => { router.push(`/search?keywords=${k}`) }}>
+                        {k}
+                    </KeywordPill>
+                ))}
+            </div>
+        )}
+    </div>)
+}
+
+
