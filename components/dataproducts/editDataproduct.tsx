836d0a49f31dfcfaad646af1a722df9415ed4f5a
Erik Vattekar <erik.vattekar@nav.no>
2022-09-13 13:07:04 +0200

    set team on update dataproduct as well

--- components/dataproducts/editDataproduct.tsx
+++ components/dataproducts/editDataproduct.tsx
@@ -33,10 +33,12 @@ const EditDataproduct = ({ product }: EditDatacollectionFormProps) => {
       },
     })
   const [productAreaID, setProductAreaID] = useState<string>('')
+  const [teamID, setTeamID] = useState<string>('')
 
   const { errors } = formState
   const onSubmit = (requestData: UpdateDataproduct) => {
     requestData.productAreaID = productAreaID
+    requestData.teamID = teamID
     updateDataproduct({
       variables: { id: product.id, input: requestData },
       awaitRefetchQueries: true,
@@ -83,6 +85,7 @@ const EditDataproduct = ({ product }: EditDatacollectionFormProps) => {
           register={register}
           errors={errors}
           setProductAreaID={setProductAreaID}
+          setTeamID={setTeamID}
         />
         <TextField
           style={{ width: '350px', display: 'block' }}
