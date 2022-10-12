3cf1221493319db7ed4a754f5b6ca6e9b2666a05
Kent D <kent.daleng@nav.no>
2022-10-12 12:59:19 +0200

    semi-hardcode po-helse into a sentence

--- components/productArea/productAreaLinks.tsx
+++ components/productArea/productAreaLinks.tsx
@@ -15,21 +15,10 @@ const ProductAreaLinks = () => {
         <div className="border border-border-muted bg-white rounded-lg w-11/12 md:w-[32rem] h-fit p-4">
             <Heading level="2" size="medium">Utforsk områder</Heading>
             <p>Er du nysgjerrig på hva de ulike områdene i NAV gjør? Her kan du utforske dataprodukter og fortellinger områdene har publisert.</p>
-            <p>Foreløpig viser vi kun en pilot.</p>
-                            
-            {productAreas.map((pa) => (
-                <div key={pa.id} className="py-2">
-                    <Link href={`/productArea/${pa.id}`} className="nada-product-area-link max-w-[47rem] align-top">
-                        <div className="flex flex-col w-full px-4 py-3">
-                            <div className="flex flex-row gap-2">
-                                <Bandage />
-                                <div className="whitespace-nowrap">{pa.name}</div>
-                            </div>
-                            <p className="whitespace-nowrap text-black">{pa.stories.length} fortellinger / {pa.dataproducts.length} produkter</p>
-                        </div>
-                    </Link>
-                </div>
-            ))}
+            <p>Foreløpig viser vi kun en pilot for {productAreas.map((pa) => (
+                <Link href={`/productArea/${pa.id}`}>{pa.name}</Link>
+                ))}
+            </p>
         </div>
     )
 }
