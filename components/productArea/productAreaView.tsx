19eb17bb83c05e17428a6c3803ebfbccaa5f0be9
Meng Zhu <meng.zhu@nav.no>
2022-10-13 15:14:09 +0200

    make product area link general

--- components/productArea/productAreaView.tsx
+++ components/productArea/productAreaView.tsx
@@ -1,31 +1,49 @@
-import { useState } from "react"
-import { PAItem, PAItems } from "../../pages/productArea/[id]"
-import ProductAreaContent from "./content"
-import ProductAreaSidebar from "./sidebar"
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { ProductAreasQuery } from '../../lib/schema/graphql'
+import { PAItem, PAItems } from '../../pages/productArea/[id]'
+import ProductAreaContent from './content'
+import ProductAreaSidebar from './sidebar'
 
 interface ProductAreaViewProps {
-    paItems: PAItems
+  paItems: PAItems
+  productAreas: ProductAreasQuery['productAreas']
 }
 
-const ProductAreaView = ({paItems}: ProductAreaViewProps) => {
-    const initialTab = (item: PAItem) => {
-        return item.dashboardURL ? "dashboard" : "stories"
-    }
+const ProductAreaView = ({ paItems, productAreas }: ProductAreaViewProps) => {
+  const initialTab = (item: PAItem) => {
+    return item.dashboardURL ? 'dashboard' : 'stories'
+  }
 
-    const [currentItem, setCurrentItem] = useState(0)
-    const [currentTab, setCurrentTab] = useState(initialTab(paItems[0]))
+  const [currentItem, setCurrentItem] = useState(0)
+  const [currentTab, setCurrentTab] = useState(initialTab(paItems[0]))
 
-    const handleSetCurrentItem = (newCurrent: number) => {
-        setCurrentItem(newCurrent)
-        setCurrentTab(initialTab(paItems[newCurrent]))
-    }
+  const handleSetCurrentItem = (newCurrent: number) => {
+    setCurrentItem(newCurrent)
+    setCurrentTab(initialTab(paItems[newCurrent]))
+  }
 
-    return (
-        <div className="flex flex-row h-full flex-grow gap-3 pt-8">
-            <ProductAreaSidebar productAreaItems={paItems} setCurrentItem={handleSetCurrentItem} currentItem={currentItem} />
-            <ProductAreaContent currentItem={paItems[currentItem]} currentTab={currentTab} setCurrentTab={setCurrentTab} />
-        </div>
-    )
+  const router = useRouter()
+  const handleSelectProductArea = (newProductAreaID:string) => {
+    router.push(`/productArea/${newProductAreaID}`)
+  }
+
+  return (
+    <div className="flex flex-row h-full flex-grow gap-3">
+      <ProductAreaSidebar
+        productAreaItems={paItems}
+        setCurrentItem={handleSetCurrentItem}
+        currentItem={currentItem}
+        productAreas={productAreas}
+        selectProductArea={handleSelectProductArea}
+      />
+      <ProductAreaContent
+        currentItem={paItems[currentItem]}
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+      />
+    </div>
+  )
 }
 
-export default ProductAreaView;
+export default ProductAreaView
--- components/productArea/sidebar.tsx
+++ components/productArea/sidebar.tsx
@@ -1,37 +1,93 @@
-import { Data } from "@navikt/ds-icons";
-import * as React from "react";
-import { PAItems } from "../../pages/productArea/[id]";
-import DataproductLogo from "../lib/icons/dataproductLogo";
+import { Data } from '@navikt/ds-icons'
+import { Select } from '@navikt/ds-react'
+import * as React from 'react'
+import { ProductAreasQuery } from '../../lib/schema/graphql'
+import { PAItems } from '../../pages/productArea/[id]'
+import DataproductLogo from '../lib/icons/dataproductLogo'
 
 interface ProductAreaSidebarProps {
-    productAreaItems: PAItems
-    setCurrentItem: (newCurrent: number) => void
-    currentItem: number
+  productAreaItems: PAItems
+  setCurrentItem: (newCurrent: number) => void
+  currentItem: number
+  productAreas: ProductAreasQuery['productAreas']
+  selectProductArea: (productAreaId: string) => void
 }
 
-const ProductAreaSidebar = ({ productAreaItems, setCurrentItem, currentItem }: ProductAreaSidebarProps) => {
-  const style = (isCurrent: boolean) => `border-l-[6px] py-1 px-2 hover:cursor-default ${isCurrent ? 'border-l-link' : 'border-l-transparent'}`
+const ProductAreaSidebar = ({
+  productAreaItems,
+  setCurrentItem,
+  currentItem,
+  productAreas,
+  selectProductArea,
+}: ProductAreaSidebarProps) => {
+  const style = (isCurrent: boolean) =>
+    `border-l-[6px] py-1 px-2 hover:cursor-default ${
+      isCurrent ? 'border-l-link' : 'border-l-transparent'
+    }`
   return (
-    <div className="flex text-base w-64 flex-col gap-2">
-        {productAreaItems.map(( d: any, idx: number) => d.stories.length || d.dataproducts.length
-          ? <div className={style(currentItem == idx)}>
-              <a className="font-semibold no-underline hover:underline" href="#" key={idx} onClick={() => setCurrentItem(idx)}>{d.name}</a>
+    <div className='pl-[1.5rem] pr-[2rem] w-96 pt-8'>
+      <Select
+        className="w-full mb-[1rem]"
+        label=""
+        onChange={(e) => selectProductArea(e.target.value)}
+        value = {productAreas.find(it=> it.name == productAreaItems[0].name)?.id}
+      >
+        {productAreas.map((it) => (
+          <option value={it.id}>{it.name}</option>
+        ))}
+      </Select>
+      <div className="flex text-base w-full flex-col gap-2">
+        {productAreaItems.map((d: any, idx: number) =>
+          d.stories.length || d.dataproducts.length ? (
+            <div className={style(currentItem == idx)}>
+              <a
+                className="font-semibold no-underline hover:underline"
+                href="#"
+                key={idx}
+                onClick={() => setCurrentItem(idx)}
+              >
+                {d.name}
+              </a>
               <div className="flex justify-between w-24">
-                <span className="flex gap-2 items-center"><Data aria-label="datafortellinger" className="text-text-muted" /> {d.stories.length}</span>
-                <span className="flex gap-2 items-center"><div className="h-[18px] w-[18px] text-text-muted"><DataproductLogo /></div> {d.dataproducts.length}</span>
+                <span className="flex gap-2 items-center">
+                  <Data
+                    aria-label="datafortellinger"
+                    className="text-text-muted"
+                  />{' '}
+                  {d.stories.length}
+                </span>
+                <span className="flex gap-2 items-center">
+                  <div className="h-[18px] w-[18px] text-text-muted">
+                    <DataproductLogo />
+                  </div>{' '}
+                  {d.dataproducts.length}
+                </span>
               </div>
             </div>
-          : <div className={style(currentItem == idx)} key={idx}>
+          ) : (
+            <div className={style(currentItem == idx)} key={idx}>
               <p className="font-semibold">{d.name}</p>
               <div className="flex justify-between w-24">
-                <span className="flex gap-2 items-center"><Data aria-label="datafortellinger" className="text-text-muted" /> {d.stories.length}</span>
-                <span className="flex gap-2 items-center"><div className="h-[18px] w-[18px] text-text-muted"><DataproductLogo /></div> {d.dataproducts.length}</span>
+                <span className="flex gap-2 items-center">
+                  <Data
+                    aria-label="datafortellinger"
+                    className="text-text-muted"
+                  />{' '}
+                  {d.stories.length}
+                </span>
+                <span className="flex gap-2 items-center">
+                  <div className="h-[18px] w-[18px] text-text-muted">
+                    <DataproductLogo />
+                  </div>{' '}
+                  {d.dataproducts.length}
+                </span>
               </div>
             </div>
-      
+          )
         )}
+      </div>
     </div>
   )
 }
 
-export default ProductAreaSidebar;
+export default ProductAreaSidebar
