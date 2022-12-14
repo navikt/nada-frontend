b407cf3398a3931c0b5d15d1f59a6c6a465771d5
Kent D <kent.daleng@nav.no>
2022-12-14 09:58:38 +0100

    redo utforsk-menu

--- components/productArea/productAreaMobileMenu.tsx
+++ components/productArea/productAreaMobileMenu.tsx
@@ -1,10 +1,9 @@
-import { Back, Data } from "@navikt/ds-icons"
+import { Back, Combifridge1, Next } from "@navikt/ds-icons"
 import { Button, Heading, Modal } from "@navikt/ds-react"
+import Divider from "@navikt/ds-react-internal/esm/dropdown/Menu/Divider"
 import { useState } from "react"
 import { ProductAreasQuery } from "../../lib/schema/graphql"
 import { PAItems } from "../../pages/productArea/[id]"
-import DataproductLogo from "../lib/icons/dataproductLogo"
-import ExploreAreasIcon from "../lib/icons/exploreAreasIcon"
 
 interface MobileMenuProps {
     open: boolean
@@ -28,32 +27,46 @@ const ProductAreaMobileMenu = ({ open, setOpen, productAreaItems, setCurrentItem
     const [selected, setSelected] = useState<string | null>(null)
 
 
-    return <Modal className="w-screen py-8" open={open} onClose={() => setOpen(false)}>
-        {!selected && <div className="flex flex-wrap mt-14 gap-2 mx-2">
-            {productAreas.map((area, idx) => <a
-                href="#"
-                onClick={() => { setSelected(area.id); selectProductArea(area.id) }}
-                key={idx}
-                className="w-[calc(50%-4px)] px-2 aspect-square border-2 rounded border-border-inverted flex flex-col justify-center items-center">
-                <Heading level="2" size="medium" className="shrink">{area.name}</Heading>
-            </a>)}</div>
+    return <Modal className="w-screen h-screen py-8" open={open} onClose={() => setOpen(false)}>
+        {!selected && <div className="flex flex-col mt-14 gap-2 mx-2">
+            {productAreas.map((area, idx) => 
+            <>
+                {idx != 0 && <Divider />}
+                <a
+                    href="#"
+                    onClick={() => { setSelected(area.id); selectProductArea(area.id) }}
+                    key={idx}
+                    className="h-10 w-full px-2 flex items-center justify-between">
+                    <Heading level="2" size="medium">{area.name}</Heading>
+                    <div>
+                        <Next className="w-6 h-6" />
+                    </div>
+                </a>
+            </>)}</div>
         }
         {selected && <div>
             <Button className="absolute top-4 left-4 h-9" variant="tertiary" icon={<Back aria-hidden />} onClick={() => setSelected(null)}>Tilbake</Button>
-            <div className="gap-2 mt-16 mx-4">
+            <div className="flex flex-col gap-2 mt-14 mx-2">
                 {productAreaItems.map((d, idx) => d.stories.length || d.dataproducts.length ? (
-                    <a
-                        href="#"
-                        onClick={() => { setCurrentItem(idx); setOpen(false)}}
-                        key={idx}
-                        className={`${idx == 0 ? "pl-0" : "pl-4"} flex`}
+                    <>
+                        {idx != 0 && <Divider />}
+                        <a
+                            href="#"
+                            onClick={() => { setCurrentItem(idx); setOpen(false) }}
+                            key={idx}
+                            className={`${idx == 0 ? "px-2" : "px-8"} flex h-10 w-full items-center justify-between`}
                         >
-                        <Heading level="2" size="medium" className="shrink">{d.name}</Heading>
-                    </a>
+                            <Heading level="2" size="medium" className="shrink">{d.name}</Heading>
+                            
+                        </a>
+                    </>
                 ) : (
-                    <div className={`${idx == 0 ? "pl-0" : "pl-4"} flex`}>
-                        <Heading level="2" size="medium" className="shrink">{d.name}</Heading>
-                    </div>
+                    <>
+                        {idx != 0 && <Divider />}
+                        <div className={`${idx == 0 ? "px-2" : "px-8"} flex h-10 w-full items-center justify-between`}>
+                            <Heading level="2" size="medium" className="shrink font-normal">{d.name}</Heading>
+                        </div>
+                    </>
                 )
                 )}
             </div>
