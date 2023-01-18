04ca99c3c047906b499cd93c43cfab9252244ee7
Kent D <kent.daleng@nav.no>
2023-01-18 11:12:44 +0100

    default to område helse, update url if query returns with data

--- components/productArea/productAreaLinks.tsx
+++ components/productArea/productAreaLinks.tsx
@@ -1,24 +1,18 @@
 import { Heading, Link } from '@navikt/ds-react'
 import { useProductAreasQuery } from '../../lib/schema/graphql'
-import ErrorMessage from '../lib/error'
 import ExploreAreasIcon from '../lib/icons/exploreAreasIcon'
 
 const ProductAreaLinks = () => {
+  var defaultProductAreaID = '6b149078-927b-4570-a1ce-97bbb9499fb6'
   const productAreasQuery = useProductAreasQuery()
 
-  if (productAreasQuery.error)
-    return <ErrorMessage error={productAreasQuery.error} />
-  if (
-    productAreasQuery.loading ||
-    !productAreasQuery.data?.productAreas ||
-    !productAreasQuery.data.productAreas.length
-  )
-    return <></>
-
-  const defaultProductAreaID =
+  if (productAreasQuery.data) {
+    defaultProductAreaID =
     productAreasQuery.data.productAreas.find(
       (it) => it.id == '6b149078-927b-4570-a1ce-97bbb9499fb6'
     )?.id || productAreasQuery.data.productAreas[0].id
+  }
+  
   return (
     <div className="border border-border-default bg-white rounded-lg w-11/12 md:w-[35rem] h-fit p-4 flex items-center flex-col md:flex-row gap-4 md:gap-0">
       <div className="mr-6">
