8f434d537ea69f1c884298cb22a29cab06629398
meng.zhu@nav.no <meng.zhu@nav.no>
2024-04-24 16:30:17 +0200

    fix alert in index

--- components/user/accessRequestAlert.tsx
+++ components/user/accessRequestAlert.tsx
@@ -4,7 +4,7 @@ import { UserState } from "../../lib/context"
 
 export const AccessRequestAlert = () => {
     const userData = useContext(UserState)
-    return userData?.accessRequests?.length ?(
+    return userData?.accessRequestsAsGranter?.length ?(
         <Alert variant='info'>
             Du har tilgangssøknad som venter på <Link href="/user/requestsForGroup">behandling</Link>.
         </Alert>): <></>
